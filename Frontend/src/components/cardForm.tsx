2f1b8c2118f8171bb3478f9ba2de74c9fff3b3cf
ayanchavand <ayan.chavand15@gmail.com>
2024-08-31 19:47:48 +0530

    feat: implemented tabs for credit and debit form

--- Frontend/src/components/cardForm.tsx
+++ Frontend/src/components/cardForm.tsx
@@ -1,41 +1,26 @@
 "use client"
 
-
-import { useState } from "react"
-
 import DebitCardForm from "./forms/debitCardForm"
 import CreditCardForm from "./forms/creditCardForm"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
-import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectGroup,
-    SelectValue,
-} from "@/components/ui/select"
 
 export default function CardForm() {
 
-    const [isCredit, setIsCredit] = useState(true)
-
     return (
-        <div className="border p-8">
-            <Select onValueChange={(value) => {
-                (value === "Credit Card") ? setIsCredit(true) : setIsCredit(false)
-                console.log(isCredit)
-            }}>
-                <SelectTrigger className="">
-                    <SelectValue placeholder="Credit Card" />
-                </SelectTrigger>
-                <SelectContent>
-                    <SelectGroup>
-                        <SelectItem value="Credit Card">Credit Card</SelectItem>
-                        <SelectItem value="Debit Card">Debit Card</SelectItem>
-                    </SelectGroup>
-                </SelectContent>
-            </Select>
-            {isCredit ? <CreditCardForm /> : <DebitCardForm />}
+        <div className="border p-8 shadow-xl">
+            <Tabs defaultValue="creditCard" className="">
+                <TabsList>
+                    <TabsTrigger value="creditCard">Credit Card</TabsTrigger>
+                    <TabsTrigger value="debitCard">Debit Card</TabsTrigger>
+                </TabsList>
+                <TabsContent value="creditCard">
+                    <CreditCardForm/>
+                </TabsContent>
+                <TabsContent value="debitCard">
+                    <DebitCardForm/>
+                </TabsContent>
+            </Tabs>
         </div>
     )
 
