9df97ca3d7dff8d3015e5489d9db0d2854a08590
ayanchavand <ayan.chavand15@gmail.com>
2024-08-31 19:48:12 +0530

    style: changed main font to thin

--- Frontend/src/components/navbar.tsx
+++ Frontend/src/components/navbar.tsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 export default function NavBar() {
     return (
         <nav className="flex justify-between items-center gap-8 mb-6">
-            <Link to="/" className="text-2xl font-bold font-mono">Bank app</Link>
+            <Link to="/" className="text-2xl font-thin font-mono">BankApp</Link>
             <ul className="">
                 <li className="flex gap-4 ">
                     <Link to="/addCard">Add Card</Link>
